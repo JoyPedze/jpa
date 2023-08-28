365b65cd521b3ec5e89c0096deacc416c6049e28
Joy Pedze <joypedped@gmail.com>
2023-08-28 02:45:06 +0200

    query methods

--- src/main/java/com/jp/jpa/JpaApplication.java
+++ src/main/java/com/jp/jpa/JpaApplication.java
@@ -20,25 +20,9 @@ public class JpaApplication {
 			Student joy = new Student("Joy", "Pedze", "joy@jp.com", 77);
 			Student tania = new Student("Tania", "Pedze", "tania@jp.com", 21);
 
-			System.out.println("Adding Joy and Tania");
 			studentRepository.saveAll(List.of(joy,tania));
 
-			System.out.print("Number of students: ");
-			studentRepository.count();
-
-			studentRepository.findById(1L).ifPresentOrElse(System.out::println,()-> System.out.println("student with ID 11 not found"));
-			studentRepository.findById(3l).ifPresentOrElse(System.out::println,()-> System.out.println("student with ID 12 not found"));
-
-			System.out.println("select all students");
-			List<Student> students = studentRepository.findAll();
-			students.forEach(System.out::println);
-
-			System.out.println("delete tania");
-			studentRepository.deleteById(2L);
-
-			System.out.print("Number of students: ");
-			System.out.println(studentRepository.count());
-
+			studentRepository.findStudentByEmail("tania@jp.com").ifPresentOrElse(System.out::println,()-> System.out.println("student not found"));
 		};
 	}
 
