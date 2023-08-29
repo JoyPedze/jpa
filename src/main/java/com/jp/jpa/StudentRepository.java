9b45636da972030a15301747e92cc79fbe228492
Joy Pedze <joypedped@gmail.com>
2023-08-29 08:33:00 +0200

    Named Parameters in StudentRepository

--- src/main/java/com/jp/jpa/StudentRepository.java
+++ src/main/java/com/jp/jpa/StudentRepository.java
@@ -2,6 +2,7 @@ package com.jp.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +17,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
     List<Student> findStudentsByFirstNameEqualsAndAgeGreaterThanEqual(String firstName, Integer age);
 
-    @Query(value = "SELECT * FROM student WHERE first_name = ?1 AND age >= ?2", nativeQuery = true)
-    List<Student> findStudentsByFirstNameEqualsAndAgeGreaterThanEqualNative(String firstName, Integer age);
+    @Query(value = "SELECT * FROM student WHERE first_name = :firstName AND age >= :age", nativeQuery = true)
+    List<Student> findStudentsByFirstNameEqualsAndAgeGreaterThanEqualNative(@Param("firstName") String firstName, @Param("age") Integer age);
 }
