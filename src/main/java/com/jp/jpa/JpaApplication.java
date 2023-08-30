9023ac5f896ecf1a939a0a979e6f08633f1e8288
Joy Pedze <joypedped@gmail.com>
2023-08-30 08:09:41 +0200

    Implemented Faker to save random students

--- src/main/java/com/jp/jpa/JpaApplication.java
+++ src/main/java/com/jp/jpa/JpaApplication.java
@@ -1,5 +1,6 @@
 package com.jp.jpa;
 
+import com.github.javafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,18 +18,26 @@ public class JpaApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository studentRepository){
 		return args -> {
-			Student joy = new Student("Joy", "Pedze", "joy@jp.com", 77);
-			Student joy1 = new Student("Joy", "Pedze", "joy1@jp.com", 78);
-			Student tania = new Student("Tania", "Pedze", "tania@jp.com", 21);
+//			Student joy = new Student("Joy", "Pedze", "joy@jp.com", 77);
+//			Student joy1 = new Student("Joy", "Pedze", "joy1@jp.com", 78);
+//			Student tania = new Student("Tania", "Pedze", "tania@jp.com", 21);
+//			studentRepository.saveAll(List.of(joy,tania,joy1));
+//			studentRepository.findStudentByEmail("tania@jp.com").ifPresentOrElse(System.out::println,()-> System.out.println("student not found"));
+//			studentRepository.whereFirstNameAndAgeEquals("Joy",77).forEach(System.out::println);
+//			studentRepository.findStudentsByFirstNameEqualsAndAgeGreaterThanEqual("Joy", 10).forEach(System.out::println);
+//			studentRepository.findStudentsByFirstNameEqualsAndAgeGreaterThanEqualNative("Joy",78).forEach(System.out::println);
+//			studentRepository.deleteStudentById(3L);
+//			studentRepository.findAll().forEach(System.out::println);
 
-			studentRepository.saveAll(List.of(joy,tania,joy1));
-
-			studentRepository.findStudentByEmail("tania@jp.com").ifPresentOrElse(System.out::println,()-> System.out.println("student not found"));
-			studentRepository.whereFirstNameAndAgeEquals("Joy",77).forEach(System.out::println);
-			studentRepository.findStudentsByFirstNameEqualsAndAgeGreaterThanEqual("Joy", 10).forEach(System.out::println);
-			studentRepository.findStudentsByFirstNameEqualsAndAgeGreaterThanEqualNative("Joy",78).forEach(System.out::println);
-			studentRepository.deleteStudentById(3L);
-			studentRepository.findAll().forEach(System.out::println);
+			Faker faker = new Faker();
+			for (int i = 0; i <= 20; i++) {
+				String firstName = faker.name().firstName();
+				String lastName = faker.name().lastName();
+				String email = String.format("%s.%s@jp.com", firstName,lastName);
+				Integer age = faker.number().numberBetween(10,80);
+				Student student = new Student(firstName,lastName,email,age);
+				studentRepository.save(student);
+			}
 		};
 	}
 
